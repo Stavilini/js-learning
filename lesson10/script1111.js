507b54f5a178667d87e3383ea1d3356a0788d8b6
Stavilini <59998273+Stavilini@users.noreply.github.com>
2020-09-09 17:06:18 +0500

    Add files via upload

---
+++ lesson10/script1111.js
@@ -0,0 +1,10 @@
+arr1 = ['apple', 'orange', 'mango', 'banana', 'dragonfruit', 'peach'];
+arr2 = ['water', 'tea', 'cola', 'sprite', 'coffee', 'beer'];
+
+// arr1.push('watermelon'); //добавляет эл-т в конец массива и возвращает новую его длину 
+// arr1.pop()// удаляет эл-т в конце массива
+// arr1.shift()// удаляет эл-т в начале массива
+// arr1.unshift('lemon')//добавляет эл-т в начало массива и возвращает новую его длину
+// arr1.join()//объединяет элементы массива в строку
+console.log(arr1.pop());
+console.log(arr1);
